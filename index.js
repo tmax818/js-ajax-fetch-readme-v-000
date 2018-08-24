fa4a1fef489d7b4a8b8a1d1e3f7a70a1ca6b53d2
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-08-24 04:31:16 -0700

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '7d7345792391189943a1344aa5e8797a6cc67fd5'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
